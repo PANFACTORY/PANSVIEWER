de617ddaafde312b45f389e699c67e998a662c19
PANFACTORY <yutatanabe510@gmail.com>
2020-09-22 13:10:22 +0900

    Add some html files

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var http = require('http');
+var fs = require('fs')
 var url = require('url');
 
 http.createServer((req, res) => {
-    let q = url.parse(req.url, true).query;
-    let msg = `${q.name} is ${q.age} years old`;
+    let pathname = url.parse(req.url).pathname;
+    console.log(`Request for ${pathname} recieved`);
 
-    res.writeHead(200, {'Content-Type' : 'text/plain'})
-    res.write(msg);
-    res.end();
+    if (pathname == '/') {
+        pathname = '/index';
+    }
+
+    fs.readFile('docs/' + pathname.substr(1), (err, data) => {
+        if (err) {
+            console.log(err);
+
+            res.writeHead(404, {'Content-Type' : 'text/plain'});
+            res.write('404 - file not found');
+        } else {
+            res.writeHead(200, {'Content-Type' : 'text/html'});
+            res.write(data.toString());
+        }
+        res.end();
+    });
 }).listen(3000);
+
+console.log('server running on port 3000');
