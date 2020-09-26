0b2f2b30273138574c7b2f975917baa042681d8f
PANFACTORY <yutatanabe510@gmail.com>
2020-09-26 11:38:14 +0900

    Add fetch

---
+++ canvasserver2D.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.urlencoded({extended : false}));
+app.use(bodyParser.json());
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/docs/index3.html');
+});
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    res.json(req.body);
+});
+
+app.listen(7000, function() {
+    console.log('app listening on port 7000');
+});
