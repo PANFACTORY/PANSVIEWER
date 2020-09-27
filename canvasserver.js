88952d425141b11440134d0ca8284410d2453d24
PANFACTORY <yutatanabe510@gmail.com>
2020-09-27 17:43:35 +0900

    Remove needless files

--- canvasserver.js
+++ canvasserver.js
@@ -1,32 +1,41 @@
 const express = require('express');
-const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT || 7000;
-const pansconv = require('./modules/pansconverter');
+const multer = require('multer');
+const fs = require('fs');
 const pansload = require('./modules/pansloader');
+const pansconv = require('./modules/pansconverter');
+const PORT = process.env.PORT || 7000;
 
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
-app.use(bodyParser.urlencoded({extended : false}));
+const app = express();
+const upload = multer({ dest : __dirname + '/uploads/' }); 
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.text());
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/docs/index3.html');
+});
 
 let vertexes = new Array();
 let faces = new Array();
 
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/docs/index2.html');
+app.post('/', upload.single('model'), function(req, res) {
+    let params = JSON.parse(req.body.params);
+    let rawdata = fs.readFileSync(__dirname + '/uploads/' + req.file.filename, 'utf-8');
+
+    [ vertexes, faces ] = pansload.loadObjectPly(rawdata);
+    
+    fs.unlinkSync(__dirname + '/uploads/' + req.file.filename);
+
+    res.send(JSON.stringify(pansconv.convertObject(vertexes, faces, params.vertexf, params.vertexa, params.h*params.r)));
 });
 
-app.post('/file', function(req, res) {
-    let param = JSON.parse(req.body.param);
-    [ vertexes, faces ] = pansload.loadObjectPly(req.body.obj);
-    res.send(JSON.stringify(pansconv.convertObject(vertexes, faces, param.vertexf, param.vertexa, param.h*param.r)));
-});
-
-app.post('/param', function(req, res) {
-    let param = JSON.parse(req.body.param);
-    res.send(JSON.stringify(pansconv.convertObject(vertexes, faces, param.vertexf, param.vertexa, param.h*param.r)));
+app.post('/params', upload.any(), function(req, res) {
+    let params = JSON.parse(req.body.params);
+    res.send(JSON.stringify(pansconv.convertObject(vertexes, faces, params.vertexf, params.vertexa, params.h*params.r)));
 });
 
 app.listen(PORT, function() {
-    console.log('Example app listening on port ' + PORT);
+    console.log('app listening on port ' + PORT);
 });
